3303af8315948b9631e83cea31532716af836748
Kirpalsinh Sarvaiya <kbsarvaiya123@gmail.com>
2024-04-08 23:50:50 +0530

    #Update

--- src/pages/baby/Weekly.jsx
+++ src/pages/baby/Weekly.jsx
@@ -21,8 +21,36 @@ import babyName from '../../assets/images/baby/baby1.png'
 import "./baby.css"
 
 export const Weekly = () => {
-    const [selectedTab, setSelectedTab] = useState("1-5 week");
+    const [selectedTab, setSelectedTab] = useState("1");
     const history = useHistory();
+
+    const tabs = [{
+        id: 1,
+        text: '1-5 Week',
+        image: require('../../assets/images/baby/1-5.png'),
+        description: '',
+        length: 0.05,
+        weight: 0
+    },
+    {
+        id: 2,
+        text: '6-10 Week',
+        image: require('../../assets/images/baby/6-10.png'),
+        description: '',
+        length: 1.22,
+        weight: 1.20
+    },
+    {
+        id: 3,
+        text: '11-15 Week',
+        image: require('../../assets/images/baby/11-15.png'),
+        description: '',
+        length: 6.60,
+        weight: 4.10
+    }
+    ]
+
+    const currentTab = tabs.find(x => x.id == selectedTab);
     return (
         <IonContent>
             <IonHeader>
@@ -33,41 +61,42 @@ export const Weekly = () => {
                 </div>
             </IonHeader>
             <IonSegment value={selectedTab} onIonChange={e => setSelectedTab(e.detail.value)}>
-                <IonSegmentButton value="1-5 week">
-                    <IonLabel>1-5 week</IonLabel>
+                <IonSegmentButton value="1">
+                    <IonLabel>1-5 Week</IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="2">
+                    <IonLabel>6-10 Week</IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="3">
+                    <IonLabel>11-15 Week</IonLabel>
                 </IonSegmentButton>
             </IonSegment>
 
-            <div className='d-flex justify-content-center'>
-                <IonCard >
-                    <img height={200} src={babyName} />
-                </IonCard>
-                <IonCard color="light"  >
-                    <IonCardHeader >
-                        <IonCardTitle><b>Size</b>   <b>&gt;</b>    </IonCardTitle>
+            {selectedTab &&
+                <> <div className='d-flex '>
+                    <IonCard >
+                        <img height={200} src={currentTab.image} />
+                    </IonCard>
+                    <IonCard color="light" style={{ width: 150 }}  >
+                        <IonCardHeader >
+                            <IonCardTitle><b>Size</b>   <b>&gt;</b>    </IonCardTitle>
 
-                        <IonCardSubtitle><b>Length</b> <br />  (inch)</IonCardSubtitle> 0.05       </IonCardHeader>
+                            <IonCardSubtitle><b>Length</b> <br />  (inch)</IonCardSubtitle> {currentTab.length}      </IonCardHeader>
 
-                    <IonCardContent className=''><b>Weight</b></IonCardContent>
-                </IonCard>
-            </div>
+                        <IonCardContent className=''><b>Weight</b> (ounces) </IonCardContent> {currentTab.weight}
+                    </IonCard>
+                </div>
 
-            <IonItem>
-                <IonLabel>
-                    <h2><b>Your Baby</b></h2>
-                    <p>
-                        During the first 1-5 weeks of your baby's life, each day unveils new wonders and challenges as you navigate the journey of parenthood. Your baby, still in the tender stages of infancy, relies on you for everything, fostering a profound bond built on love and care.
-                    </p><p>
-                        In these initial weeks, your baby's development is remarkable. They gradually adjust to life outside the womb, exhibiting reflexes like rooting and sucking, essential for feeding and comfort. Their senses awaken, responding to your touch, voice, and scent, strengthening the attachment between parent and child.
-                    </p><p>
-                        Despite their tiny size, your baby's personality begins to emerge, manifesting through subtle cues and expressions. You'll witness their first fleeting smiles, a heartwarming glimpse into their budding emotions.
-                    </p><p>
-                        Amidst the joys of parenthood, challenges may arise, such as adjusting to feeding schedules, deciphering your baby's cries, and managing sleep patterns. However, each obstacle is an opportunity for growth, strengthening your confidence and intuition as a caregiver.
-                    </p><p>
-                        As you embark on this transformative journey, cherish the quiet moments of bonding with your newborn, knowing that each cuddle, kiss, and gentle lullaby nurtures their physical and emotional well-being. These precious weeks lay the foundation for a lifetime of love, trust, and cherished memories between you and your little one.
-                    </p>
-                </IonLabel>
-            </IonItem>
+                    <IonItem>
+                        <IonLabel>
+                            <h2><b>Your Baby</b></h2>
+                            <p>
+                                {currentTab.description}
+                            </p>
+                        </IonLabel>
+                    </IonItem>
+                </>
+            }
 
 
 
