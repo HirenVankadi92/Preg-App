3e33b137fb4b4e632b21e020509e17e1447c4b49
Kirpalsinh Sarvaiya <kbsarvaiya123@gmail.com>
2024-04-08 23:04:23 +0530

    #Chnages

--- src/pages/baby/Size.jsx
+++ src/pages/baby/Size.jsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonIcon,  IonSegment, IonSegmentButton, IonList, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonSelect, IonSelectOption,IonButton} from '@ionic/react'
-import { chevronBackOutline, personCircleOutline,call, heart, pin } from 'ionicons/icons'
+import { IonContent, IonHeader, IonIcon, IonSegment, IonSegmentButton, IonList, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonSelect, IonSelectOption, IonButton } from '@ionic/react'
+import { chevronBackOutline, personCircleOutline, call, heart, pin } from 'ionicons/icons'
 import React, { useState } from 'react'
 import { useHistory } from 'react-router'
 import "./baby.css"
@@ -9,99 +9,84 @@ import Tabs from '../../components/Tabs'
 import BabyNames from '../../assets/images/baby/Apple seed.png'
 
 export const Size = () => {
-    const [selectedNavItem, setSelectedNavItem] = useState("");
+  const [selectedNavItem, setSelectedNavItem] = useState("");
 
   // Function to handle nav item click
   const handleNavItemClick = (navItem) => {
     setSelectedNavItem(navItem);
   };
-    const history = useHistory();
-    const images = [
-      "Chia seed",
-      "Watermelon seed",
-      "Chia seed",
-      "blueberry",
-      "raspberry",
-      "Apple seed",
-      "Watermelon seed",
-      "blueberry",
-      "raspberry",
-      "graps",
-      "strawberry",
-      "fig",
-      "Kiwi",
-      "Avocado",
-      "Mango",
-      "banana",
-      "coconut",
-      "Bunch of graps",
-      "Pineapple",
-      "Rhubarb",
-      "cantaloupe",
-      "jackfruit"
+  const history = useHistory();
+  const images = [
+    "Chia seed",
+    "Watermelon seed",
+    "Chia seed",
+    "blueberry",
+    "raspberry",
+    "Apple seed",
+    "Watermelon seed",
+    "blueberry",
+    "raspberry",
+    "graps",
+    "strawberry",
+    "fig",
+    "Kiwi",
+    "Avocado",
+    "Mango",
+    "banana",
+    "coconut",
+    "Bunch of graps",
+    "Pineapple",
+    "Rhubarb",
+    "cantaloupe",
+    "jackfruit"
   ];
-  
+
   const items = images.map((image, index) => ({
-      id: index, // Start ids from 3 since we are skipping 1 and 2
-      image: image,
-      name: image
+    id: index, // Start ids from 3 since we are skipping 1 and 2
+    image: image,
+    name: image
   }));
 
   const imagesPath = items.map(item => require(`../../assets/images/baby/${item.image}.png`));
 
-    const renderNavLinks = () => {
-        const links = [];
-        for (let i = 1; i <= 40; i += 2) {
-          links.push(
-            <IonButton
-              key={i}
-              className="nav-link"
-              onClick={() => console.log()}
-            >
-              {i}-{i + 1}
-            </IonButton>
-          );
-        }
-        return links;
-      };
-    return (
-        <IonContent>
-            <IonHeader>
-                <div className="header py-3 px-3 d-flex justify-content-between align-items-center">
-                    <IonIcon icon={chevronBackOutline} onClick={() => history.push("/baby")} style={{ fontSize: "24px", color: "white" }} />
-                    <p className="m-0 fw-bold fs-5 text-white">Size</p>
-                    <IonIcon icon={personCircleOutline} style={{ fontSize: "24px", color: "white" }} />
-                </div>
-            </IonHeader>
+  return (
+    <IonContent>
+      <IonHeader>
+        <div className="header py-3 px-3 d-flex justify-content-between align-items-center">
+          <IonIcon icon={chevronBackOutline} onClick={() => history.push("/baby")} style={{ fontSize: "24px", color: "white" }} />
+          <p className="m-0 fw-bold fs-5 text-white">Size</p>
+          <IonIcon icon={personCircleOutline} style={{ fontSize: "24px", color: "white" }} />
+        </div>
+      </IonHeader>
 
-            <IonSegment>
-          {['<1', '1-2', '3-4', 5, 6, 7, 8, 9, 10,11,12,13,14,15].map(navItem => (
-            <IonSegmentButton 
-              key={navItem} 
-              value={navItem} 
-              onClick={() => handleNavItemClick(navItem.toString())} 
-              className={selectedNavItem === navItem.toString() ? 'segment-button active' : 'segment-button'}
-            >
-              {navItem}
-              <br></br>
-              {selectedNavItem === navItem.toString() ? "Week" : ""}
-            </IonSegmentButton>
-          ))}
-        </IonSegment>
+      <IonSegment mode='md'>
+        {['<1', '1-2', '3-4', 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(navItem => (
+          <IonSegmentButton
+            key={navItem}
+            value={navItem}
+            onClick={() => handleNavItemClick(navItem.toString())}
+            className={selectedNavItem === navItem.toString() ? 'segment-button active' : 'segment-button'}
+          >
+            {navItem}
+            <br></br>
+            {selectedNavItem === navItem.toString() ? "Week" : ""}
+          </IonSegmentButton>
+        ))}
+      </IonSegment>
 
-        {selectedNavItem && imagesPath[selectedNavItem] && items[selectedNavItem].name  && (<>
-          <div className="d-flex justify-content-center align-items-center">
-    <div className="img">
-      {/* Display the image using the selected item index */}
-      <img className="w-100 h-100" src={imagesPath[items[selectedNavItem].id]} alt={items[selectedNavItem].name} />
-    </div>
-  </div>
-          <div className="d-flex justify-content-center align-items-center">
-    {items[selectedNavItem].name}
-  </div>
-        </>
-  
-)}
-        </IonContent>
-    )
+      {selectedNavItem && imagesPath[selectedNavItem] && items[selectedNavItem].name && (<>
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="img">
+            {/* Display the image using the selected item index */}
+            <img className="w-100 h-100" src={imagesPath[items[selectedNavItem].id]} alt={items[selectedNavItem].name} />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center align-items-center">
+          {items[selectedNavItem].name}
+        </div>
+      </>
+
+      )}
+    </IonContent>
+  )
 }
--- src/pages/baby/Weekly.jsx
+++ src/pages/baby/Weekly.jsx
@@ -1,10 +1,27 @@
-import { IonContent, IonHeader, IonIcon } from '@ionic/react'
+import {
+    IonContent, IonHeader, IonIcon,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
+    IonItem, IonList,
+    IonInput,
+    IonTextarea,
+    IonNote,
+    IonToggle,
+    IonCardTitle,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonCard
+} from '@ionic/react'
 import { chevronBackOutline, personCircleOutline } from 'ionicons/icons'
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router'
+import babyName from '../../assets/images/baby/baby1.png'
 import "./baby.css"
 
 export const Weekly = () => {
+    const [selectedTab, setSelectedTab] = useState("1-5 week");
     const history = useHistory();
     return (
         <IonContent>
@@ -15,7 +32,45 @@ export const Weekly = () => {
                     <IonIcon icon={personCircleOutline} style={{ fontSize: "24px", color: "white" }} />
                 </div>
             </IonHeader>
-            <div>nensi</div>
+            <IonSegment value={selectedTab} onIonChange={e => setSelectedTab(e.detail.value)}>
+                <IonSegmentButton value="1-5 week">
+                    <IonLabel>1-5 week</IonLabel>
+                </IonSegmentButton>
+            </IonSegment>
+
+            <div className='d-flex justify-content-center'>
+                <IonCard >
+                    <img height={200} src={babyName} />
+                </IonCard>
+                <IonCard color="light"  >
+                    <IonCardHeader >
+                        <IonCardTitle><b>Size</b>   <b>&gt;</b>    </IonCardTitle>
+
+                        <IonCardSubtitle><b>Length</b> <br />  (inch)</IonCardSubtitle> 0.05       </IonCardHeader>
+
+                    <IonCardContent className=''><b>Weight</b></IonCardContent>
+                </IonCard>
+            </div>
+
+            <IonItem>
+                <IonLabel>
+                    <h2><b>Your Baby</b></h2>
+                    <p>
+                        During the first 1-5 weeks of your baby's life, each day unveils new wonders and challenges as you navigate the journey of parenthood. Your baby, still in the tender stages of infancy, relies on you for everything, fostering a profound bond built on love and care.
+                    </p><p>
+                        In these initial weeks, your baby's development is remarkable. They gradually adjust to life outside the womb, exhibiting reflexes like rooting and sucking, essential for feeding and comfort. Their senses awaken, responding to your touch, voice, and scent, strengthening the attachment between parent and child.
+                    </p><p>
+                        Despite their tiny size, your baby's personality begins to emerge, manifesting through subtle cues and expressions. You'll witness their first fleeting smiles, a heartwarming glimpse into their budding emotions.
+                    </p><p>
+                        Amidst the joys of parenthood, challenges may arise, such as adjusting to feeding schedules, deciphering your baby's cries, and managing sleep patterns. However, each obstacle is an opportunity for growth, strengthening your confidence and intuition as a caregiver.
+                    </p><p>
+                        As you embark on this transformative journey, cherish the quiet moments of bonding with your newborn, knowing that each cuddle, kiss, and gentle lullaby nurtures their physical and emotional well-being. These precious weeks lay the foundation for a lifetime of love, trust, and cherished memories between you and your little one.
+                    </p>
+                </IonLabel>
+            </IonItem>
+
+
+
         </IonContent>
     )
 }
